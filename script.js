e652a41c001eb5eedad7367b67be2b51efe2c2ce
Gabriel <gabriel_aires@estudante.sesisenai.org.br>
2026-02-18 10:18:47 -0300

    fix inputTime

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const display = document.getElementById('timerDisplay');
-const imputTime = document.getElementById('timeInput');
+const inputTime = document.getElementById('timeInput');
 const btnStart = document.getElementById('btnStart');
 
 console.log(display, inputTime, btnStart);
