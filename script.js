bcca4cf3d171e0e5d37b145db95e94d75eec1eb7
Victor Gabriel Aires da Silva <gabriel_aires@estudante.sesisenai.org.br>
2026-02-19 09:58:45 -0300

    Click (#6)

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-
 const display = document.getElementById('timerDisplay');
 const inputTime = document.getElementById('timeInput');
 const btnStart = document.getElementById('btnStart');
 
 console.log(display, inputTime, btnStart);
+
+btnStart.addEventListener('Click', function(){
+    let minutes = Number(inputTime.value);
+    console.log('O úsuario deseja estudar: ', minutes, ' minutos');
+});
