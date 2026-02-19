6ca8f8e9818954e9c11964c2e245f394b41e5408
Victor Gabriel Aires da Silva <gabriel_aires@estudante.sesisenai.org.br>
2026-02-18 21:52:19 -0300

    new p, session (#5)

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
     <style>
         body { background-color: #121212; color: white; font-family: sans-serif; text-align: center; padding-top: 50px; }
         h1 { font-size: 50px; }
+        #session 
+        {
+             color: yellow;
+             font-weight: bold;
+             font-size: 20px;
+        }
     </style>
 </head>
 <body>
@@ -14,6 +20,7 @@
     <p>Se você está vendo isso, o PHP funcionou!</p>
 
    <h1 id="timerDisplay">00:00</h1>
+   <P id="session">Digite quantos minutos será sua sessão de estudos: </P>
    <input type="number" id="timeInput">
    <button id="btnStart">Start</button>
 
