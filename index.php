7087f962a734cd40e5bf2bf076eb611f20a460f4
Victor Gabriel Aires da Silva <gabriel_aires@estudante.sesisenai.org.br>
2026-02-20 10:33:25 -0300

    feat: Timer and CSS (#8)

--- index.php
+++ index.php
@@ -4,14 +4,21 @@
     <meta charset="UTF-8">
     <title>Pomodoro Fighter</title>
     <style>
-        body { background-color: #121212; color: white; font-family: sans-serif; text-align: center; padding-top: 50px; }
+        body { background-color: #121212; color: white; font-family: sans-serif; text-align: center; padding-top: 10px; }
         h1 { font-size: 50px; }
+        #timerDisplay {
+            font-size: 120px;
+            border: 5px solid white; 
+            border-radius: 100px; /* Deixa a borda redonda (quanto maior o número, mais redonda) */
+            padding: 20px 40px; /* Espaço interno (20px em cima/baixo, 40px nas laterais) para a borda não esmagar os números */
+            display: inline-block; /* Impede que a borda estique até o final da tela, forçando ela a abraçar apenas o texto */
+            }
         #session 
         {
              color: yellow;
              font-weight: bold;
              font-size: 20px;
-        }
+    }
     </style>
 </head>
 <body>
